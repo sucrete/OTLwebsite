974c2698a151beb2e2b3e6e2632f7d1a042a2593
William Andree <williamandree@gmail.com>
2020-06-18 23:32:07 -0500

    building OTL structure

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Old Town Lofts</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read
+          <Link href="/posts/ferstPost">
+            <a> this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -54,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +209,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
