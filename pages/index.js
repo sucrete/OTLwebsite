1add4b8ded04c7d443995d58191cacc140ec1640
William Andree <williamandree@gmail.com>
2020-06-30 12:00:54 -0500

    adding footer and basic fonts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -23,8 +22,8 @@ export default function Home() {
             <div className="OTLogo"></div>
             <div className="KCLogo"></div>
           </div>
-          <div className="buttonContainer">
-            <a href="#footer">Application</a>
+          <a href="#footer" className="buttonContainer">
+            <span>More Info</span>
             <svg
               className="svgArrow"
               ns="http://www.w3.org/2000/svg"
@@ -32,7 +31,7 @@ export default function Home() {
             >
               <path d="M268.65 301.745C268.65 312.258 267.118 324.427 264.05 338.266 261.235 350.567 259.834 360.946 259.834 369.403L260.983 369.403C269.926 347.875 279.444 331.604 289.542 320.583 299.635 309.566 312.092 299.054 326.916 289.06L348 314.048C326.276 325.838 309.152 338.396 296.633 351.72 284.109 365.042 273.118 382.473 263.667 404L248.334 404C238.875 382.473 227.885 365.042 215.367 351.72 202.841 338.396 185.718 325.838 164 314.048L185.084 289.06C199.902 299.054 212.36 309.566 222.458 320.583 232.551 331.604 242.068 347.875 251.017 369.403L252.167 369.403C252.167 361.714 250.885 351.468 248.334 338.649 246.8 331.477 245.65 325.26 244.884 320.006 244.118 314.755 243.734 308.666 243.734 301.745L243.734 108 268.65 108 268.65 301.745Z" />
             </svg>
-          </div>
+          </a>
         </div>
         {/* 
         <div className="grid">
@@ -65,9 +64,32 @@ export default function Home() {
           </a>
         </div> */}
       </main>
+      <footer id="footer">
+        <svg viewBox="0 0 1198 30" className="footerSVG">
+          <path d="M-1,30C-1,30,599,30,599,30C599,30,599,19.33,599,0C453.66,0,374.09,14.24,199.67,22.97C89.21,28.49,-1,30,-1,30C-1,30,-1,30,-1,30M1199,30C1199,30,599,30,599,30C599,30,599,19.33,599,0C744.34,0,823.91,14.24,998.33,22.97C1108.79,28.49,1199,30,1199,30C1199,30,1199,30,1199,30"></path>
+        </svg>
 
-      <footer id="footer">Buncha junk</footer>
-      <svg
+        <div className="footerContent">
+          <h1>Contact</h1>
+          <div className="contact">
+            phone number: (816) 842-1424 <br />
+            <br /> address:
+            <a
+              className="appButton"
+              href="https://www.oldtownloftskc.com/files/5215/9249/4482/Application_2020_Editable.pdf"
+              target="_blank"
+            >
+              Application{" "}
+              <img className="appImg" src="/file.svg" alt="my image" />
+            </a>
+          </div>
+          <img
+            style={{ float: "right" }}
+            src="https://www.oldtownloftskc.com/files/cache/d61cd69cec4da7d8ff35c0192b01d635_f23.png"
+          />
+        </div>
+      </footer>
+      {/* <svg
         className="hiddenSVG"
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
@@ -88,7 +110,7 @@ export default function Home() {
             <feComposite in="SourceGraphic" in2="goo" operator="atop" />
           </filter>
         </defs>
-      </svg>
+      </svg> */}
 
       <style jsx global>{``}</style>
     </div>
