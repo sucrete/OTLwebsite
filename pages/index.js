df5b023b1bb735ee3e043515f064b3b79e933574
William Andree <williamandree@gmail.com>
2020-07-11 01:06:39 -0500

    changed marquee form to create better flow

--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,12 @@ export default function Home() {
               <div className="OTLogo"></div>
               <div className="KCLogo"></div>
             </div>
-            <a href="#footer" className="buttonContainer">
-              <span>Contact</span>
-              <svg
-                className="svgArrow"
-                ns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M268.65 301.745C268.65 312.258 267.118 324.427 264.05 338.266 261.235 350.567 259.834 360.946 259.834 369.403L260.983 369.403C269.926 347.875 279.444 331.604 289.542 320.583 299.635 309.566 312.092 299.054 326.916 289.06L348 314.048C326.276 325.838 309.152 338.396 296.633 351.72 284.109 365.042 273.118 382.473 263.667 404L248.334 404C238.875 382.473 227.885 365.042 215.367 351.72 202.841 338.396 185.718 325.838 164 314.048L185.084 289.06C199.902 299.054 212.36 309.566 222.458 320.583 232.551 331.604 242.068 347.875 251.017 369.403L252.167 369.403C252.167 361.714 250.885 351.468 248.334 338.649 246.8 331.477 245.65 325.26 244.884 320.006 244.118 314.755 243.734 308.666 243.734 301.745L243.734 108 268.65 108 268.65 301.745Z" />
-              </svg>
-            </a>
           </div>
+          <div className="quoteContainer">
+            <div className="OTLQuoteLine1">"Affordable living in the</div>{" "}
+            <div className="OTLQuoteLine2">heart of Kansas City."</div>
+          </div>
+
           <div className="OTLLogoContainer">
             <img className="drawing" src="/drawing.svg" />
             <img className="circle" src="/circle.svg" />
@@ -74,6 +69,17 @@ export default function Home() {
             </div>
             <div className="KCtext2">KC</div>
           </div>
+          <div className="transitionGradient"></div>
+          <a href="#footer" className="buttonContainer">
+            <span>Our Properties</span>
+            <svg
+              className="svgArrow"
+              ns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M268.65 301.745C268.65 312.258 267.118 324.427 264.05 338.266 261.235 350.567 259.834 360.946 259.834 369.403L260.983 369.403C269.926 347.875 279.444 331.604 289.542 320.583 299.635 309.566 312.092 299.054 326.916 289.06L348 314.048C326.276 325.838 309.152 338.396 296.633 351.72 284.109 365.042 273.118 382.473 263.667 404L248.334 404C238.875 382.473 227.885 365.042 215.367 351.72 202.841 338.396 185.718 325.838 164 314.048L185.084 289.06C199.902 299.054 212.36 309.566 222.458 320.583 232.551 331.604 242.068 347.875 251.017 369.403L252.167 369.403C252.167 361.714 250.885 351.468 248.334 338.649 246.8 331.477 245.65 325.26 244.884 320.006 244.118 314.755 243.734 308.666 243.734 301.745L243.734 108 268.65 108 268.65 301.745Z" />
+            </svg>
+          </a>
         </div>
       </main>
       <footer id="footer">
@@ -114,11 +120,20 @@ export default function Home() {
           </div>
           <div className="freepikContainer">
             Some icons made by
-            <a href="http://www.freepik.com/" title="Freepik">
+            <a
+              className="freepik"
+              href="http://www.freepik.com/"
+              title="Freepik"
+              target="_blank"
+            >
               Freepik
             </a>
             from
-            <a href="https://www.flaticon.com/" title="Flaticon">
+            <a
+              href="https://www.flaticon.com/"
+              target="_blank"
+              title="Flaticon"
+            >
               www.flaticon.com
             </a>
           </div>
