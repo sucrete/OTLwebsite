08097fece4c2d08f87769a5ea3b38b482bf0928b
William Andree <williamandree@gmail.com>
2020-07-14 00:23:29 -0500

    in process of adding styling to cards

--- components/Cards.js
+++ components/Cards.js
@@ -8,19 +8,19 @@ const breakpointColumnsObj = {
 };
 
 const cards = [
-  { zone: "River Market", building: "Chase Lofts", url: "", picURL: "chaseLofts", color: "azure" },
-  { zone: "River Market", building: "Old Town Lofts", url: "", picURL: "oldTownLofts", color: "azure" },
-  { zone: "Business Loop", building: "917 Lofts", url: "", picURL: "917Lofts", color: "cornflowerblue" },
-  { zone: "Business Loop", building: "Waltower Lofts", url: "", picURL: "waltowerLofts", color: "cornflowerblue" },
-  { zone: "Business Loop", building: "Hanover Lofts", url: "", picURL: "hanoverLofts", color: "cornflowerblue" },
-  { zone: "Business Loop", building: "Graphic Arts", url: "", picURL: "graphicArts", color: "cornflowerblue" },
-  { zone: "Business Loop", building: "Chambers Lofts", url: "", picURL: "chambersLofts", color: "cornflowerblue" },
-  { zone: "Business Loop", building: "Finance Lofts", url: "", picURL: "financeLofts", color: "cornflowerblue" },
-  { zone: "Crossroads", building: "Columbia Lofts", url: "", picURL: "columbiaLofts", color: "#CADB72" },
-  { zone: "Crossroads", building: "Carriage Lofts", url: "", picURL: "carriageLofts", color: "#CADB72" },
-  { zone: "Crossroads", building: "Star Lofts", url: "", picURL: "starLofts", color: "#CADB72" },
-  { zone: "", building: "Wellington", url: "", picURL: "wellington", color: "tomato" },
-  { zone: "", building: "Lucas Place", url: "", picURL: "lucasPlace", color: "#BEA2F2" }
+  { district: "River Market", building: "Chase Lofts", url: "", picURL: "chaseLofts", color: "azure" },
+  { district: "River Market", building: "Old Town Lofts", url: "", picURL: "oldTownLofts", color: "azure" },
+  { district: "Business Loop", building: "917 Lofts", url: "", picURL: "917Lofts", color: "cornflowerblue" },
+  { district: "Business Loop", building: "Waltower Lofts", url: "", picURL: "waltowerLofts", color: "cornflowerblue" },
+  { district: "Business Loop", building: "Hanover Lofts", url: "", picURL: "hanoverLofts", color: "cornflowerblue" },
+  { district: "Business Loop", building: "Graphic Arts", url: "", picURL: "graphicArts", color: "cornflowerblue" },
+  { district: "Business Loop", building: "Chambers Lofts", url: "", picURL: "chambersLofts", color: "cornflowerblue" },
+  { district: "Business Loop", building: "Finance Lofts", url: "", picURL: "financeLofts", color: "cornflowerblue" },
+  { district: "Crossroads", building: "Columbia Lofts", url: "", picURL: "columbiaLofts", color: "#CADB72" },
+  { district: "Crossroads", building: "Carriage Lofts", url: "", picURL: "carriageLofts", color: "#CADB72" },
+  { district: "Crossroads", building: "Star Lofts", url: "", picURL: "starLofts", color: "#CADB72" },
+  { district: "", building: "Wellington", url: "", picURL: "wellington", color: "tomato" },
+  { district: "", building: "Lucas Place", url: "", picURL: "lucasPlace", color: "#BEA2F2" }
 ];
 const Cards = () => (
   <div >
@@ -28,10 +28,11 @@ const Cards = () => (
       breakpointCols={breakpointColumnsObj}
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column">
-      {/* array of JSX items */}
       {cards.map((card) => (
         <div className="card" >
-          <div> {card.building}</div>
+          <a><div className="building"> {card.building}</div>
+            <div className="district"> {card.district}</div></a>
+
         </div>)
 
       )}
