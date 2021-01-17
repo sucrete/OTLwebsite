fbed0ceb526f3868a5f714e17eb919aa8ed18cdd
William Andree <williamandree@gmail.com>
2021-01-17 03:29:29 -0600

    I'm gettin there. Optimizing incompatibility alert card.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <title>Old Town Lofts</title>
         <link rel="icon" href="/brickwall.svg" />
       </Head>
